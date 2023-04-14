fb9d279987df5bf919dda17bc06418ba9e8f25be
NikishDAniel <nikishdaniel77@gmail.com>
2023-04-14 20:57:33 +0530

    updated

--- week9/reverse.py
+++
@@ -1,8 +0,0 @@
-'''message = "My Name is Sam"
-print(''.join(reversed(message)))'''
-
-def rreverse(message):
-    reversedString = message[::-1]
-    return reversedString
-reverseString = rreverse(input("Enter the string to be reversed : "))
-print(reverseString)
