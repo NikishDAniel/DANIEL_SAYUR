3fa0173a8e8bd32632446bc0f684ac7483eb6046
NikishDAniel <nikishdaniel77@gmail.com>
2023-05-02 20:18:32 +0530

    updated

--- week9/bowlingscoring.py
+++ week9/bowlingscoring.py
@@ -1,20 +1,29 @@
 '''plan 
     -- input two chances'''
-    
+# function to spare  
 def spare(totalRoll):
+    # in spare there is one roll chance 
     oneChance = int(input())
-    totalPoint = totalRoll + oneChance   
+    # adding the spare points + chance points 
+    totalPoint = totalRoll + oneChance 
+    # returning the total spare point  
     return totalPoint
+
+# a function to take two roll chances
 def twoRoll():
     first = int(input())
     second = int(input())
     strike = first + second
+    # returning the total strike point  
     return strike
+
+# a function to strike 
 def strike(firstRoll):
     strikePoint = firstRoll
+    # a function call to tworoll function
     total = twoRoll()
-    totalPoint = strikePoint + total  
-    return totalPoint
+    totalStrikePoint = strikePoint + total  
+    return totalStrikePoint
 def roll(): 
     totalPoint = 0
     firstRoll = int(input()) 
@@ -26,10 +35,8 @@ def roll():
         totalRoll = firstRoll+secondRoll
         if totalRoll == 10:
             totalPoint = spare(totalRoll)
-            #
         elif totalRoll < 10:
             totalPoint = totalRoll
-            #return totalPoint
         else:
             roll()
     else:
@@ -38,7 +45,7 @@ def roll():
     
 def main():
     totalPoints = 0
-    for i in range(11):
+    for i in range(10):
         totalPoints +=roll()
     return totalPoints
 output = main()
--- week9/cafenew.py
+++ week9/cafenew.py
@@ -7,7 +7,16 @@ f = mysql.connector.connect(
 )
 mycursor = f.cursor()
 mycursor.execute("select*from cafe")
-for i in mycursor:
-    print(i)
+'''def showMenu():
+    for i in mycursor:
+        print(i[1])'''
+    
+customers = 10
+while customers!= 0 :
+    for i in mycursor:
+        print(i[1])
+    for i in range(customers):
+        print(showMenu())
+    
 '''mycursor.execute("CREATE TABLE Fruit_Details (fruit_id INTEGER, name VARCHAR(255)
 , color VARCHAR(255), cost DECIMAL(5,2), city VARCHAR(255))")'''
