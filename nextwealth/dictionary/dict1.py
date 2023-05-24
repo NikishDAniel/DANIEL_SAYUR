36e70bcf2f61abba27eaf847677edac969f5d50a
NikishDAniel <nikishdaniel77@gmail.com>
2023-05-24 21:21:19 +0530

    new

---
+++ nextwealth/dictionary/dict1.py
@@ -0,0 +1,13 @@
+########## Program 1
+#find the list unique words in a sentence
+#hint - Each word is a key, count is the value
+
+sentence = "This is a cat and it has a tail and two eyes"
+countOfWords = 0
+sameChar = []
+for word in sentence:
+    if word 
+   
+print(countOfWords)
+
+########## Program 2
