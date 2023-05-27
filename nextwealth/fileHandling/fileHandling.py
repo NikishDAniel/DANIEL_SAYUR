ba31cf285279bb38f88698b165702c05d132d986
NikishDAniel <nikishdaniel77@gmail.com>
2023-05-27 22:02:17 +0530

    new

---
+++ nextwealth/fileHandling/fileHandling.py
@@ -0,0 +1,13 @@
+openFile = open("C:/Users/Nikish daniel/Desktop/DANIEL_SAYUR/nextwealth/fibbo.py","r")
+with open("C:/Users/Nikish daniel/Desktop/DANIEL_SAYUR/nextwealth/fibbo.py") as opened: 
+    newFile = open("C:/Users/Nikish daniel/Desktop/DANIEL_SAYUR/nextwealth/new.txt","w")
+    count = 0
+    for lines in opened:
+        countOfChar = 0
+        for words in lines:
+            countOfChar += 1
+        count +=1
+        newFile.write(f"Line {count} contains {countOfChar} count of words \n")
+    newFile.write(f"\ntotal Lines = {count}")
+openFile.close()
+newFile.close()
