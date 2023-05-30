e5d7cb03ffa81d9809c237b254ce8d8c17fe46a0
NikishDAniel <nikishdaniel77@gmail.com>
2023-05-30 10:48:44 +0530

    updated

--- nextwealth/nestedLoops/chess2.py
+++ nextwealth/nestedLoops/chess2.py
@@ -5,18 +5,18 @@
 
 
 # initialise the values 
-fi = "\u25A0 B "*4
-la = "B \u25A0 "*4
+first = "\u25A0 B "*4
+last = "B \u25A0 "*4
 print("----CHESS BOARD----")
 # loops for each loops 
 for rows in range(8):
     
     # printing the first variable if it is even
     if rows %2 == 0:
-        print(fi)
+        print(first)
     # else printing this 
     else:
-        print(la)
+        print(last)
         
 '''
 output ---
